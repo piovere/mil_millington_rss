926a6010108d9a7896b0ac35c196d7404d419ec5
JR Powers-Luhn <floobyt@gmail.com>
2014-12-25 19:16:32 -0800

    added steps to authorize bot and fetch tweets from mil

--- mil_twitter_rss.py
+++ mil_twitter_rss.py
@@ -3,3 +3,16 @@
 supported by a file in the same directory called "auth_keys.py" with twitter api keys
 """
 
+import auth_keys
+import twitter
+
+auth = twitter.OAuth(token=auth_keys.access_token, 
+	                 token_secret=auth_keys.token_secret,
+	                 consumer_secret=auth_keys.consumer_secret,
+	                 consumer_key=auth_keys.consumer_key)
+
+t = twitter.Twitter(auth=auth)
+
+my_tweets = t.statuses.user_timeline(screen_name="MilMillington")
+
+print my_tweets[0]['text']
