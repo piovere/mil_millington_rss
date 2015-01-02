3106ab0f453cf65b72d9b4ad99f0173b3461887f
JR Powers-Luhn <jrpowersluhn@intercap-e.com>
2015-01-02 10:13:50 -0800

    Started the RSS generation part of the code

--- mil_twitter_rss.py
+++ mil_twitter_rss.py
@@ -5,7 +5,8 @@ supported by a file in the same directory called "config.py" with twitter api ke
 
 import config
 import twitter
-import PyRSS2Gen
+import PyRSS2Gen as RSS
+import datetime
 
 auth = twitter.OAuth(token=config.access_token, 
 	                 token_secret=config.token_secret,
@@ -27,3 +28,28 @@ def url_sub(tweet):
 
 print "\n"
 print url_sub(my_tweets[0]['text'])
+
+rssitems = []
+
+for tweet in my_tweets:
+    rssitems.append(
+               RSS.RSSItem(
+                           title=tweet[key]
+                           link=tweet[key]
+                           description=tweet[key]
+                           guid="Don't know what this is supposed to be guess who gets to read the RSS spec"
+                           pubDate=tweet[key]
+                           )
+               )
+
+rss = RSS.RSS2(
+               title="Mil's Mail announcements",
+               link="https://twitter.com/MilMillington",
+               description="An announcement feed to try and keep track of when "
+                           "Mil Millington writes what used to be emails",
+                lastBuildDate=datetime.datetime.utcnow(),
+
+                items=rssitems
+                )
+
+rss.write_xml(open("milsmail.rss", "w"))
